c1dcfee8ea4ee2c22dcebc93d0e2bb66da45b541
caipenpen <66503739+caipenpen@users.noreply.github.com>
2020-07-13 17:48:00 +0700

    Update http.py

--- tools/L7/http.py
+++ tools/L7/http.py
@@ -24,9 +24,9 @@ def flood(target,domainname,attack,sock):
     }
     s = requests.Session()
     payload = str(random._urandom(random.randint(10, 500)))
-	if sock =='http':
+    if sock =='http':
 		proxies = {'http': "socks5://127.0.0.1:9050"}
-	elif sock =='https':
+    elif sock =='https':
 		proxies = {'https': "socks5h://127.0.0.1:9050"}
     else:
 		proxies =''
